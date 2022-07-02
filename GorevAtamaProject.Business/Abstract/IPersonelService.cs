c67367118c8f5c685ceb507934b1c713b0c12527
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-07-02 18:00:31 +0300

    Migration ve Update database işlemleri

---
+++ GorevAtamaProject.Business/Abstract/IPersonelService.cs
@@ -0,0 +1,18 @@
+﻿using GorevAtamaProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorevAtamaProject.Business.Abstract
+{
+    public interface IPersonelService
+    {
+        Personel GetById(int id);
+        List<Personel> GetAll();
+        void Create(Personel entity);
+        void Update(Personel entity);
+        void Delete(Personel entity);
+    }
+}
---
+++ GorevAtamaProject.Business/Concrete/PersonelManager.cs
@@ -0,0 +1,45 @@
+﻿using GorevAtamaProject.Business.Abstract;
+using GorevAtamaProject.Data.Abstract;
+using GorevAtamaProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorevAtamaProject.Business.Concrete
+{
+    public class PersonelManager : IPersonelService
+    {
+        private IPersonelRepository _personelRepository;
+
+        public PersonelManager(IPersonelRepository personelRepository)
+        {
+            _personelRepository = personelRepository;
+        }
+        public void Create(Personel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Personel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Personel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Personel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Personel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
